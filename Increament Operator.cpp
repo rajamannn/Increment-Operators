0000e258063a5b0c1ae4f6422ff51540992502cc
Aman Raj <160530800+rajamannn@users.noreply.github.com>
2024-11-06 00:31:41 +0530

    Add files via upload

---
+++ Increament Operator.cpp
@@ -0,0 +1,15 @@
+//Program to print the increment operator
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+    int var = 440;
+    cout << "Value before increment: " << var << endl;
+    var++;
+    cout << "Value after increment: " << var;
+    return 0;
+}
